77b299b53a0858257bcb330c8232b728b3c83430
remunds <raban.emunds@gmail.com>
2023-11-22 12:44:46 +0000

    wip

--- EinsumNetworks/src/try_stuff.py
+++ EinsumNetworks/src/try_stuff.py
@@ -2,14 +2,13 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from EinsumNetwork import Graph, EinsumNetwork
-# import datasets
-from sklearn.datasets import fetch_california_housing
+import numpy as np
+import os
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 ##########################################################
-dataset = 'accidents'
 
 depth = 3
 num_repetitions = 10
@@ -23,124 +22,174 @@ online_em_stepsize = 0.05
 
 ##########################################################
 
-# # train_x_orig, test_x_orig, valid_x_orig = datasets.load_debd(dataset, dtype='float32')
-# data, target = fetch_california_housing(return_X_y=True) # ((20640, 8), (20640,))
-
-# # split 70/20/10
-# train_x = data[:14448]
-# valid_x = data[14448:18576]
-# test_x = data[18576:]
-# train_y = target[:14448]
-# valid_y = target[14448:18576]
-# test_y = target[18576:]
-
-# # to torch
-# train_x = torch.from_numpy(train_x).to(torch.device(device), dtype=torch.float32)
-# valid_x = torch.from_numpy(valid_x).to(torch.device(device), dtype=torch.float32)
-# test_x = torch.from_numpy(test_x).to(torch.device(device), dtype=torch.float32)
-# train_y = torch.from_numpy(train_y).to(torch.device(device), dtype=torch.float32)
-# valid_y = torch.from_numpy(valid_y).to(torch.device(device), dtype=torch.float32)
-# test_y = torch.from_numpy(test_y).to(torch.device(device), dtype=torch.float32)
-
-
-# train_N, num_dims = train_x.shape
-# valid_N = valid_x.shape[0]
-# test_N = test_x.shape[0]
-
-# print(train_x.shape) # 12758, 111
-
 from torchvision.datasets import MNIST 
 from torchvision.transforms import ToTensor
 from torch.utils.data import DataLoader
-     
+
+batchsize_resnet = 64
 train_ds = MNIST("mnist", train=True, download=True, transform=ToTensor())
 test_ds = MNIST("mnist", train=False, download=True, transform=ToTensor())
-train_dl = DataLoader(train_ds, batch_size=64, shuffle=True, pin_memory=True, num_workers=1)
-test_dl = DataLoader(test_ds, batch_size=64, pin_memory=True, num_workers=1)
+train_dl = DataLoader(train_ds, batch_size=batchsize_resnet, shuffle=True, pin_memory=True, num_workers=1)
+test_dl = DataLoader(test_ds, batch_size=batchsize_resnet, pin_memory=True, num_workers=1)
 print("train_dl", len(train_dl))
 print("test_dl", len(test_dl))
 
 print("done loading data")
 
 ###############################################################################
-# load NN
+exists = os.path.isfile("latent_train.npy") and os.path.isfile("latent_test.npy") and os.path.isfile("target_train.npy") and os.path.isfile("target_test.npy")
 
-#TODO: extend ResNet instead of wrapping it -> no need to specify get_hidden
+if exists:
+    print("loading latent dataset")
+    latent_train = np.load("latent_train.npy")
+    target_train = np.load("target_train.npy")
+    latent_test = np.load("latent_test.npy")
+    target_test = np.load("target_test.npy")
+    print("Latent train dataset shape: ", latent_train.shape)
 
-class ResNetHidden(nn.Module):
-    """
-    ResNet model with the last layer removed
-    """
-    def __init__(self, original_model):
-        super(ResNetHidden, self).__init__()
-        self.features = nn.Sequential(*list(original_model.children())[:-1])
-        self.original_model = original_model
-        # adapt for mnist -> channel=1
-        original_model.conv1 = torch.nn.Conv2d(1, 64,
-            kernel_size=(7, 7),
-            stride=(2, 2),
-            padding=(3, 3), bias=False)
-    
-    def get_hidden(self, x):
-        for name, module in self.features.named_children():
-            if name != "fc":
-                x = module(x)
-        return x
-    
-    def forward(self, x):
-        self.original_model(x)
+if not exists:
+    # load NN
 
-from torchvision.models.resnet import resnet18
+    #TODO: extend ResNet instead of wrapping it -> no need to specify get_hidden
+    from torchvision.models.resnet import ResNet, BasicBlock
 
-resnet = ResNetHidden(resnet18(num_classes=10))
+    class ResNetHidden(ResNet):
+        """
+        ResNet model with ability to return latent space 
+        """
+        def __init__(self, *args, **kwargs):
+            super(ResNetHidden, self).__init__(*args, **kwargs)
+            # adapt for mnist -> channel=1
+            self.conv1 = torch.nn.Conv2d(1, 64,
+                kernel_size=(7, 7),
+                stride=(2, 2),
+                padding=(3, 3), bias=False)
+        
+        def get_hidden(self, x):
+            for name, module in self.named_children():
+                if name != "fc":
+                    x = module(x)
+            return x
 
-# adapt for mnist -> channel=1
-# resnet.conv1 = torch.nn.Conv2d(1, 64, 
-#     kernel_size=(7, 7), 
-#     stride=(2, 2), 
-#     padding=(3, 3), bias=False)
+    resnet = ResNetHidden(BasicBlock, [2, 2, 2, 2], num_classes=10)
 
+    resnet.to(device)
 
-resnet.to(device)
+    # train NN 
+    optimizer = torch.optim.Adam(resnet.parameters(), lr=0.001)
+    criterion = torch.nn.CrossEntropyLoss()
 
-# train NN 
-optimizer = torch.optim.Adam(resnet.parameters(), lr=0.001)
-criterion = torch.nn.CrossEntropyLoss()
+    print("start training resnet")
 
-print("start training")
+    for epoch in range(1):
+        for data, target in train_dl:
+            optimizer.zero_grad()
 
-for epoch in range(1):
-    for data, target in train_dl:
-        optimizer.zero_grad()
+            data, target = data.to(device), target.to(device)
+            output = resnet(data)
+            loss = criterion(output, target)
+            loss.backward()
+            optimizer.step() 
+        print(f"Epoch {epoch}, loss {loss.item()}")
 
-        data, target = data.to(device), target.to(device)
-        output = resnet(data)
-        loss = criterion(output, target)
-        loss.backward()
-        optimizer.step() 
-    print(f"Epoch {epoch}, loss {loss.item()}")
+    # evaluate NN
+    resnet.eval()
+    loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_dl:
+            data, target = data.to(device), target.to(device)
+            output = resnet(data)
+            loss += criterion(output, target).item()
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
 
-# evaluate NN
-resnet.eval()
-loss = 0
-with torch.no_grad():
-    for data, target in test_dl:
-        data, target = data.to(device), target.to(device)
-        output = resnet(data)
-        loss += criterion(output, target).item()
-    loss /= len(test_dl)
-    print(f"Test loss: {loss}")
+        loss /= len(test_dl)
+        print(f"Test loss: {loss}")
+        print(f"Test accuracy: {correct / len(test_dl.dataset)}")
+
+    ##########################################################
+    # collect latent space dataset
+    latent_train = np.zeros((train_ds.data.shape[0], 512))
+    target_train = np.zeros((train_ds.data.shape[0],))
+    latent_test = np.zeros((test_ds.data.shape[0], 512))
+    target_test = np.zeros((test_ds.data.shape[0],))
+    with torch.no_grad():
+        for batch_idx, (data, target) in enumerate(train_dl):
+            data = data.to(device)
+            target = target.to(device)
+            low_idx = batch_idx * batchsize_resnet
+            high_idx = (batch_idx + 1) * batchsize_resnet
+            latent_train[low_idx:high_idx] = resnet.get_hidden(data).squeeze().cpu().numpy()
+            target_train[low_idx:high_idx] = target.cpu().numpy()
+        np.save("latent_train.npy", latent_train)
+        np.save("target_train.npy", target_train)
+        print("Latent train dataset shape: ", latent_train.shape)
+
+        for batch_idx, (data, target) in enumerate(test_dl):
+            data = data.to(device)
+            target = target.to(device)
+            low_idx = batch_idx * batchsize_resnet
+            high_idx = (batch_idx + 1) * batchsize_resnet
+            latent_test[low_idx:high_idx] = resnet.get_hidden(data).squeeze().cpu().numpy()
+            target_test[low_idx:high_idx] = target.cpu().numpy()
+        np.save("latent_test.npy", latent_test)
+        np.save("target_test.npy", target_test)
+    print("done collecting latent space dataset")
+
 
 ##########################################################
-# collect latent space dataset
-latent = torch.zeros((len(train_dl.dataset), 512), dtype=torch.float32, device=device)
-for batch in range(train_x.shape[0] // batch_size):
-    low_idx = batch * batch_size
-    high_idx = (batch + 1) * batch_size
-    latent[low_idx:high_idx] = simple_nn.get_latent(train_x[low_idx:high_idx]) 
+# train simple MLP on latent space for mnist classification to show that latent space is useful
+# mlp = nn.Sequential(
+#     nn.Linear(512, 100),
+#     nn.ReLU(),
+#     nn.Linear(100, 10),
+#     nn.LogSoftmax(dim=1)
+# )
 
-print("Latent dataset shape: ", latent.shape)
+# mlp.to(device)
 
+# # train MLP
+# optimizer = torch.optim.Adam(mlp.parameters(), lr=0.001)
+# criterion = torch.nn.CrossEntropyLoss()
+
+# latent_train = torch.from_numpy(latent_train).to(dtype=torch.float32)
+# target_train = torch.from_numpy(target_train).to(dtype=torch.long)
+# latent_test = torch.from_numpy(latent_test).to(dtype=torch.float32)
+# target_test = torch.from_numpy(target_test).to(dtype=torch.long)
+
+
+# print("start training mlp")
+# for epoch in range(10):
+#     for batch_idx in range(0, latent_train.shape[0], batchsize_resnet):
+#         optimizer.zero_grad()
+
+#         data = latent_train[batch_idx:batch_idx+batchsize_resnet]
+#         target = target_train[batch_idx:batch_idx+batchsize_resnet]
+#         data, target = data.to(device), target.to(device)
+#         output = mlp(data)
+#         loss = criterion(output, target)
+#         loss.backward()
+#         optimizer.step()
+#     print(f"Epoch {epoch}, loss {loss.item()}")
+
+# # evaluate MLP
+# mlp.eval()
+# loss = 0
+# correct = 0
+# with torch.no_grad():
+#     for batch_idx in range(0, latent_test.shape[0], batchsize_resnet):
+#         data = latent_test[batch_idx:batch_idx+batchsize_resnet]
+#         target = target_test[batch_idx:batch_idx+batchsize_resnet]
+#         data, target = data.to(device), target.to(device)
+#         output = mlp(data)
+#         loss += criterion(output, target).item()
+#         pred = output.argmax(dim=1, keepdim=True)
+#         correct += pred.eq(target.view_as(pred)).sum().item()
+
+#     loss /= len(test_dl)
+#     print(f"Test loss: {loss}")
+#     print(f"Test accuracy: {correct / len(test_dl.dataset)}")
 
 ##########################################################
 # EinsumNetwork
@@ -150,7 +199,7 @@ print("Latent dataset shape: ", latent.shape)
 graph = Graph.random_binary_trees(num_var=100, depth=depth, num_repetitions=num_repetitions)
 
 args = EinsumNetwork.Args(
-    num_classes=1,
+    num_classes=10,
     num_input_distributions=num_input_distributions,
     exponential_family=EinsumNetwork.CategoricalArray,
     exponential_family_args={'K': 2},
