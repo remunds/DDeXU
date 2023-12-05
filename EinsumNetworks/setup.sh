afbfc9a3dc115d17b2f775bf5615043ba53a78fd
remunds <raban.emunds@stud.tu-darmstadt.de>
2023-12-05 14:22:47 +0000

    rm einsumnetwork

--- EinsumNetworks/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -rf venv/
-virtualenv --system-site-packages -p python3.6 ./venv
-source ./venv/bin/activate
-pip3 install --upgrade pip
-pip3 install -r requirements.txt
-cd src
-python datasets.py
